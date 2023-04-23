3e689bb6e5a178df348d339ead63d854b2a7f73c
FlaringK <caelans@iinet.net.au>
2023-04-23 09:08:37 +0800

    Change transition time

--- custom.js
+++ custom.js
@@ -8,6 +8,6 @@ const jsonURL = "https://zara-nerd.github.io/jari-data/adventure.json"
 
 setInterval(() => {
   if (document.querySelector("#video111")) {
-    if (document.querySelector("#video111").currentTime > 5) document.body.className += " black"
+    if (document.querySelector("#video111").currentTime > 144.5) document.body.className += " black"
   }
 }, 100);
