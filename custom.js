7450ea8fdbffab0a7e2c613460078ff9d6921476
FlaringK <caelans@iinet.net.au>
2023-04-23 09:05:22 +0800

    Page111 theme transition

--- custom.js
+++ custom.js
@@ -1,7 +1,13 @@
 const jsonURL = "https://zara-nerd.github.io/jari-data/adventure.json"
 
-window.onload = () => {
-  MSPFA.slide.push(() => {
-    console.log("vriska")
-  })
-}
+// window.onload = () => {
+//   MSPFA.slide.push(() => {
+//     console.log("vriska")
+//   })
+// }
+
+setInterval(() => {
+  if (document.querySelector("#video111")) {
+    if (document.querySelector("#video111").currentTime > 5) document.body.className += " black"
+  }
+}, 100);
