e24043dc3b0d25c27e163fed97263ec1cd3ea22e
FlaringK <caelans@iinet.net.au>
2023-04-24 09:28:44 +0800

    Themes

--- custom.js
+++ custom.js
@@ -8,8 +8,13 @@ const jsonURL = "https://zara-nerd.github.io/jari-data/adventure.json"
 
 setInterval(() => {
   if (document.querySelector("#video111")) {
-    if (document.querySelector("#video111").currentTime > 144.5 && document.querySelector("#video111").currentTime < 241) document.body.className = "mspfa p111 dark"
-    else document.body.className = "mspfa p111"
+    let vidTime = document.querySelector("#video111").currentTime
+    
+    document.body.className = "mspfa p111"
+    if (vidTime > 144.5 && vidTime < 241) document.body.className = "mspfa p111 dark"
+    if (vidTime > 67 && vidTime < 144.5) document.body.className = "mspfa p111 sunset"
+    if (vidTime > 117.5 && vidTime < 144.5) document.body.className = "mspfa p111 twilight"
+    if (vidTime > 121.5 && vidTime < 144.5) document.body.className = "mspfa p111 nightsky"
     console.log(document.querySelector("#video111").currentTime > 144.5)
   }
 }, 100);
