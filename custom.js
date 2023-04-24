698696fbc25949bdd28f125df38c7261552ed6fc
FlaringK <caelans@iinet.net.au>
2023-04-24 09:49:55 +0800

    Widescreen

--- custom.js
+++ custom.js
@@ -11,10 +11,12 @@ setInterval(() => {
     let vidTime = document.querySelector("#video111").currentTime
     
     document.body.className = "mspfa p111"
-    if (vidTime > 144.5 && vidTime < 241) document.body.className = "mspfa p111 dark"
     if (vidTime > 67 && vidTime < 144.5) document.body.className = "mspfa p111 sunset"
-    if (vidTime > 117.5 && vidTime < 144.5) document.body.className = "mspfa p111 twilight"
+    if (vidTime > 117. && vidTime < 144.5) document.body.className = "mspfa p111 twilight"
     if (vidTime > 121.5 && vidTime < 144.5) document.body.className = "mspfa p111 nightsky"
+    if (vidTime > 138 && vidTime < 144.5) document.body.className = "mspfa p111 nightsky widescreen"
+    if (vidTime > 144.5 && vidTime < 241) document.body.className = "mspfa p111 dark widescreen"
+    if (vidTime > 238 && vidTime < 241) document.body.className = "mspfa p111 dark"
     console.log(document.querySelector("#video111").currentTime > 144.5)
   }
 }, 100);
