5981491d2f68d813d13985b2f686d07ed59f1669
FlaringK <caelans@iinet.net.au>
2023-04-24 09:01:36 +0800

    Timing

--- custom.js
+++ custom.js
@@ -8,6 +8,8 @@ const jsonURL = "https://zara-nerd.github.io/jari-data/adventure.json"
 
 setInterval(() => {
   if (document.querySelector("#video111")) {
-    if (document.querySelector("#video111").currentTime > 144.5) document.body.className += " black"
+    if (document.querySelector("#video111").currentTime > 144.5 && document.querySelector("#video111").currentTime < 241) document.body.className = "mspfa p111 dark"
+    else document.body.className = "mspfa p111"
+    console.log(document.querySelector("#video111").currentTime > 144.5)
   }
 }, 100);
